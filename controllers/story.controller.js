f89957c2b9b611199138d3bc30a6e601e424eb4c
BrandonGilmore <beatallica51@gmail.com>
2021-10-27 11:04:50 -0500

    added new controller fxns

--- controllers/story.controller.js
+++ controllers/story.controller.js
@@ -1,7 +1,20 @@
+const Character = require('../models/characterModels');
+const Story = require('../models/storyModels');
+
 exports.storyParty = async (req, res) => {
-  
-}//return the GeneralNarrative obj from (new?) db
+    let story = await Story.findById(req.params.id);
+    res.json(story);
+}
 
 exports.storyCharacter = async (req, res) => {
-
-}//return character specific history from db; only assoc user / char
+    try {
+        const character = await Character.findById(req.params.id);
+        if (!character) {
+          res.status(404).send();
+        }
+        res.json(character.story);
+      }
+      catch(error) {
+        res.status(500).send();
+      }
+}
